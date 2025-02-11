23b486e10b84307c5f26592a34ce19b9a3b89340
Oscar A <oscar@alshammar.se>
2025-02-11 21:08:01 +0100

    Added Player texture/ img, mouse aim and debugs hitbox, gun bullet spawn

--- Player.cs
+++ Player.cs
@@ -1,30 +1,67 @@
 using Raylib_cs;
+using System.Numerics;
 
+class Player
+{
+    public int X { get; set; } = Raylib.GetScreenWidth() / 2;
+    public int Y { get; set; } = Raylib.GetScreenHeight() / 2;
+    public int Radius { get; set; } = 40;
+    public Raylib_cs.Color Color { get; set; } = Color.Blue;
 
-class Player{
-    public int X {get; set;} = Raylib.GetScreenWidth() / 2;
-    public int Y {get; set;} = Raylib.GetScreenHeight() / 2;
-    public int Radius {get;set;} = 20;
-    public Raylib_cs.Color Color {get;set;} = Color.Blue;
-    
-    public int Hp {get; set;} = 100;
-    public int Speed {get; set;} = 5;
-    public int FireRate {get; set;} = 5;
-    public int Damage {get; set;} = 20;
+    public int Hp { get; set; } = 100;
+    public int Speed { get; set; } = 5;
+    public int FireRate { get; set; } = 5;
+    public int Damage { get; set; } = 20;
 
+    public Texture2D texture;
+    public float Scale { get; set; } = 0.5f;
 
-    public void Update(){
+    public float GunOffsetX { get; set; } = 90;
+    public float GunOffsetY { get; set; } = 20;
+
+    public Player()
+    {
+        texture = Raylib.LoadTexture("player.png");
+    }
+
+    public void Update()
+    {
         Movement();
     }
 
-    public void Draw() {
-        Raylib.DrawCircle(X, Y,Radius, Color);
+    public void Draw()
+    {
+        Raylib.DrawCircle(X, Y, Radius, Color); // Debug Hitbox
+
+    
+        Vector2 playerPosition = new Vector2(X, Y);
+        Vector2 mousePosition = Raylib.GetMousePosition();
+        float angle = MathF.Atan2(mousePosition.Y - playerPosition.Y, mousePosition.X - playerPosition.X) * (180.0f / MathF.PI);
+
+        Rectangle sourceRec = new Rectangle(0, 0, texture.Width, texture.Height);
+
+        Rectangle destRec = new Rectangle(X, Y, texture.Width * Scale, texture.Height * Scale);
+
+        Vector2 origin = new Vector2((texture.Width * Scale / 2) - 27, texture.Height * Scale / 2);
+        Raylib.DrawTexturePro(texture, sourceRec, destRec, origin, angle, Color.White);
+
+        
+        float gunX = X + GunOffsetX * MathF.Cos(angle * MathF.PI / 180) - GunOffsetY * MathF.Sin(angle * MathF.PI / 180);
+        float gunY = Y + GunOffsetX * MathF.Sin(angle * MathF.PI / 180) + GunOffsetY * MathF.Cos(angle * MathF.PI / 180);
+        Raylib.DrawCircle((int)gunX, (int)gunY, 5, Color.Red); // Debug bullet spawn
+
+
     }
 
-    void Movement(){
-        if(Raylib.IsKeyDown(KeyboardKey.W)) Y -= Speed;
-        if(Raylib.IsKeyDown(KeyboardKey.S)) Y += Speed;
-        if(Raylib.IsKeyDown(KeyboardKey.D)) X += Speed;
-        if(Raylib.IsKeyDown(KeyboardKey.A)) X -= Speed;
+    void Movement()
+    {
+        if (Raylib.IsKeyDown(KeyboardKey.W)) Y -= Speed;
+        if (Raylib.IsKeyDown(KeyboardKey.S)) Y += Speed;
+        if (Raylib.IsKeyDown(KeyboardKey.D)) X += Speed;
+        if (Raylib.IsKeyDown(KeyboardKey.A)) X -= Speed;
+
+
+        X = Math.Clamp(X, 0 + Radius, Raylib.GetScreenWidth() - Radius);
+        Y = Math.Clamp(Y, 0 + Radius, Raylib.GetScreenHeight() - Radius);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
-﻿using MongoDB.Bson.IO;
-using Raylib_cs;
+﻿using Raylib_cs;
 
 class Program
 {
     static void Main()
     {
 
-        Raylib.InitWindow(800,600, "Sigma game");
+        Raylib.InitWindow(1200,800, "Sigma game");
         Player p = new();
+        Bush[] bushes = new Bush[5];
+        for (int i = 0; i < bushes.Length; i++)
+        {
+            bushes[i] = new Bush();
+        }
+
 
         while(!Raylib.WindowShouldClose()){
             p.Update();
@@ -16,10 +21,25 @@ class Program
             Raylib.BeginDrawing();
             Raylib.ClearBackground(Color.Green);
             p.Draw();
+            foreach(Bush b in bushes)
+                b.Draw();
             Raylib.EndDrawing();
 
         }
+        Raylib.UnloadTexture(p.texture);
+        Raylib.CloseWindow();
 
 
+    }
+}
+
+
+class Bush{
+    public int Radius = Random.Shared.Next(10, 50);
+    public int X = Random.Shared.Next(Raylib.GetScreenWidth());
+    public int Y = Random.Shared.Next(Raylib.GetScreenHeight());
+
+    public void Draw(){
+        Raylib.DrawCircle(X,Y, Radius, Color.DarkGreen);
     }
 }
